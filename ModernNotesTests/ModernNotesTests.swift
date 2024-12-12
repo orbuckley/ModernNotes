30291ba23b33078423ac7d7654ce04ab7d446ff1
Owen Buckley <owenbuckley@me.com>
2024-12-12 21:02:11 +0000

    Initial Commit

---
+++ ModernNotesTests/ModernNotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ModernNotesTests.swift
+//  ModernNotesTests
+//
+//  Created by Owen Buckley on 12/12/2024.
+//
+
+import Testing
+@testable import ModernNotes
+
+struct ModernNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
